692e77442727110b1a288a3bea9c7c50fd648768
Sohum Shah <sohum.shah@thetalentpool.co.in>
2025-02-05 12:49:33 +0530

    config.py changes to make the prompt  light weight

--- config.py
+++ config.py
@@ -4,12 +4,14 @@ system_prompt = """
 You are tasked with extracting and organizing personal, academic, and employment information from candidate resumes. Your goal is to identify relevant details and categorize them into sections such as personal information, educational background, skills, and employment history.
 Give all dates in dd/MM/yyyy format.
 Stick to the given json template only.
-Ensure the output is structured clearly, suitable for further processing in recruitment workflows, and always include the WorkedPeriod calculation, ensuring accuracy.
+Provide the output as valid JSON. Do not include any markdown formatting, such as triple backticks or other extra characters. Respond with only the JSON content.
 """
+# Ensure the output is structured clearly, suitable for further processing in recruitment workflows, and always include the WorkedPeriod calculation, ensuring accuracy.
+# """
 
 user_prompt = "User is requesting to extract the following details from the resume text:"
 
-json_template = {
+json_template_old = {
     "Address": [
         {
             "City": "",
@@ -74,6 +76,41 @@ json_template = {
         }
     ],
     "Objective": ""
+}
+
+json_template = {
+    "City": "",
+    "PersonalDetails": {
+        "Name": {"FirstName": "", "LastName": "", "MiddleName": "", "FullName": "", "TitleName": ""},
+        "DateOfBirth": "",
+        "Mobile": [],
+        "Email": [],
+        "Nationality": ""
+    },
+    "Academics": [
+        {
+            "Degree": "",
+            "Branch": "",
+            "StartDate": "",
+            "EndDate": "",
+            "Institute": "",
+            "Score": ""
+        }
+    ],
+    "CurrentEmployer": "",
+    "CurrentSalary": "",
+    "ExpectedSalary": "",
+    "WorkedPeriod": {"TotalExperienceInMonths": "", "TotalExperienceInYear": "",
+                     "TotalExperienceRange": ""},
+    "Skills": [],
+    "WorkExperience": [
+        {
+            "Organization": "",
+            "StartDate": "",
+            "EndDate": "",
+            "Designation": ""
+        }
+    ]
 }
 
 OPENAI_API_KEY='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
